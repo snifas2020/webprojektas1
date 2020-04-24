2bd025f0dc3a005f448c67da3d7cb83b96b7caa4
RimanteG <rimante.lukosiute@gmail.com>
2020-04-24 22:44:44 +0300

    contacts update

--- contacts.php
+++ contacts.php
@@ -1,52 +1,38 @@
 <?php include "header.php" ?>
 
+
 <div class="container">
-
-    <div class="row">
-
-        <div class="col s12 m6 l6">
-            <h5 class="contactHeader">Contact Us</h5>
-            <div>
-                <form>
-                    <input type="text" name="" placeholder="Full name">
-                    <input type="text" name="" value="programavimas@info.lt">
-                    <textarea placeholder="Messege"></textarea>
-                    <button>SEND</button>
-                </form>
-            </div>
-        </div>
-
-        <div class="col s12 m6 l6">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2305.9098246088492!2d25.266742816069158!3d54.69361448064311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd940648ab4aa5%3A0xb37b0ef00dbfeca4!2sAlaus%20Namai!5e0!3m2!1slt!2slt!4v1587754973917!5m2!1slt!2slt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-        </div>
-
-
+    <div style="text-align:center">
+        <h2>Contact Us</h2>
+        <p>Swing by for a cup of coffee, or leave us a message:</p>
     </div>
-
+    <div class="row">
+        <div class="column">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2305.9098246088492!2d25.266742816069158!3d54.69361448064311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd940648ab4aa5%3A0xb37b0ef00dbfeca4!2sAlaus%20Namai!5e0!3m2!1slt!2slt!4v1587754973917!5m2!1slt!2slt" width="100%" height="403" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        <div class="column">
+            <form action="/action_page.php">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name..">
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+                <label for="country">Country</label>
+                <select id="country" name="country">
+                    <option value="australia">Australia</option>
+                    <option value="canada">Canada</option>
+                    <option value="usa">USA</option>
+                </select>
+                <label for="subject">Subject</label>
+                <textarea id="subject" name="subject" placeholder="Write something.." style="height:170px"></textarea>
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
 </div>
 
 
-<!-- <div class="container">
-    <div class="page-section bg-blue">
-        <div class="content-wrapper">
-            <h2 class="h-white">Contact Us</h2>
 
-            <div class="blocks-wrapper">
-                <div class="form-wrapper">
-                    <form>
-                        <input type="text" name="" placeholder="Full name">
-                        <input type="text" name="" value="programavimas@info.lt">
-                        <textarea placeholder="Messege"></textarea>
-                        <button>SEND</button>
-                    </form>
-                </div>
-                <div class="map-wrapper">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.983423849919!2d25.290128216029665!3d54.70991477940151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3+g.+5%2C+Vilnius+08240!5e0!3m2!1sen!2slt!4v1530566996484" width="100%" height="403" frameborder="0" style="border:0" allowfullscreen></iframe>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
+
 <!-- Čia kontaktų puslapio vidinė dalis -->
 
 
