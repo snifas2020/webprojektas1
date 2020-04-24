d8da765376f069ead00776a46852adce4998e95a
snifas2020 <serpenticad@gmail.com>
2020-04-24 18:48:49 +0300

    JG - ikeliau lenteles sukurimo koda

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 session_start();
 $host = "localhost"; /* Host name */
 $user = "root"; /* User */
-$password = "Testas12"; /* Password */
+$password = "";/*"Testas12"; Password */
 $dbname = "projektas_1"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
@@ -10,3 +10,10 @@ $con = mysqli_connect($host, $user, $password,$dbname);
 if (!$con) {
  die("Connection failed: " . mysqli_connect_error());
 }
+
+/*
+JG lenteles sukurimo sql kodas:
+Atsiudaryti db ir sql tabe ikelti zemiau esancia eilute. Paleisti ja paspaudus go.
+
+CREATE TABLE `projektas_1`.`charts` ( `chartsid` INT NOT NULL AUTO_INCREMENT , `java` DECIMAL(10,2) NOT NULL , `csharp` DECIMAL(10,2) NOT NULL , `css` DECIMAL(10,2) NOT NULL , `javascript` DECIMAL(10,2) NOT NULL , `php` DECIMAL(10,2) NOT NULL , `chartscode` TINYINT NOT NULL , PRIMARY KEY (`chartsid`)) ENGINE = InnoDB;
+*/
