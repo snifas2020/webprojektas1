5042160b156849e4259e5c0d370167b65a41b42d
RimanteG <rimante.lukosiute@gmail.com>
2020-04-24 22:10:49 +0300

    contacts

--- contacts.php
+++ contacts.php
@@ -13,7 +13,8 @@
                 </form>
             </div>
             <div class="map-wrapper">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2304.983423849919!2d25.290128216029665!3d54.70991477940151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd96ae1dedd637%3A0x73978617430c730c!2sUlon%C5%B3+g.+5%2C+Vilnius+08240!5e0!3m2!1sen!2slt!4v1530566996484" width="100%" height="403" frameborder="0" style="border:0" allowfullscreen></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2305.9098246088492!2d25.266742816069158!3d54.69361448064311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd940648ab4aa5%3A0xb37b0ef00dbfeca4!2sAlaus%20Namai!5e0!3m2!1slt!2slt!4v1587754973917!5m2!1slt!2slt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
             </div>
         </div>
     </div>
