32a90620eef3770be565743a6912a65fc6ee425f
mokymasMOKYMAS <64151554+mokymasMOKYMAS@users.noreply.github.com>
2020-04-27 14:12:32 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,24 +1,12 @@
-
 <?php 
 include "config.php";
+include "header.php";
+
 ?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Registracijos forma į mokymus</title>
-
-<!-- laikinas dizainas-->
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-    <!-- Bootstrap JS -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
-
-
   </head>
   <body>
     <div class='container'>
@@ -53,38 +41,83 @@ include "config.php";
             }
             ?>
 
+
+
+
             <div class="form-group">
               <label for="fname">Vardas:</label>
               <input type="text" class="form-control" name="fname" id="fname" required="required" maxlength="80">
             </div>
+
             <div class="form-group">
               <label for="lname">Pavardė:</label>
               <input type="text" class="form-control" name="lname" id="lname" required="required" maxlength="80">
             </div>
+
             <div class="form-group">
               <label for="email">Pašto adresas:</label>
               <input type="email" class="form-control" name="email" id="email" required="required" maxlength="80">
             </div>
+           
             <div class="form-group">
-              <label for="password">Slaptažodis:</label>
-              <input type="password" class="form-control" name="password" id="password" required="required" maxlength="80">
-            </div>
+            <label for="text">Artimiausios mokymų datos:</label><br>
+
+            
+            <p>2020-05-05</p>
+            <p>2020-07-05</p>
+            <p>2020-09-05</p>
+            
+
+          </div>
+           
+         
+
+            
+
+
+
             <div class="form-group">
-              <label for="pwd">Pakartokite slaptažodį:</label>
-              <input type="password" class="form-control" name="confirmpassword" id="confirmpassword" onkeyup='' required="required" maxlength="80">
+              <label for="phone">Telefono numeris:</label>
+              <p>+370 <input  type="phone" class="form-control" name="phone" id="phone" required="required" maxlength="8"></p>
             </div>
 
-            <button type="submit" name="btnsignup" class="btn btn-default">Registruotis</button><br>
+            
+            
+            
+            
 
-            <br><div class="g-recaptcha" data-sitekey="6LcePAATAAAAAGPRWgx90814DTjgt5sXnNbV5WaW"></div>
+          
+            <br><button type="submit" href="message.php" name="btnsignup" class="btn btn-default">Registruotis<br></button>
+           <?php
+            if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['fname'];
+    $last_name = $_POST['lname'];
+    $subject = "registracija i konsultacijas";
+   
+    $message = $first_name . " " . $last_name . " nori uzsiregistruoti i mokymus:" . "\n\n" . $_POST['message'];
+     mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+    ?>
+        
 
-            <script src='https://www.google.com/recaptcha/api.js'></script>
 
+            <br><script src='https://www.google.com/recaptcha/api.js?hl=es'></script>
+<br><div class="g-recaptcha" data-sitekey="6Ldk7e4UAAAAAGyDs2ieQuTS9h8rDgYiKppHGmth"></div>
            
           </form>
+          
         </div>
 
+        
      </div>
     </div>
+    <?php
+    include "footer.php";
+    ?>
   </body>
 </html>
