ef5179b1435a29da0059a3b7407ff66f539c51a0
mokymasMOKYMAS <64151554+mokymasMOKYMAS@users.noreply.github.com>
2020-04-27 15:58:31 +0300

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,3 +1,5 @@
+<?php include "header.php" ?>
+=======
 <?php 
 include "config.php";
 include "header.php";
@@ -11,113 +13,128 @@ include "header.php";
   <body>
     <div class='container'>
       <div class='row'>
-
-        <div class='col-md-6' >
-
-          <form method='post' action=''>
-
-            <h1>Registracijos forma į konsultacijas</h1>
-            <?php 
-            // Klaidos pranesimas
-            if(!empty($error_message)){
-            ?>
-            <div class="alert alert-danger">
-              <strong>Įvyko klaida!</strong> <?= $error_message ?>
-            </div>
-
-            <?php
-            }
-            ?>
-
-            <?php 
-            // jei pavyksta prisijungti išmetamas pranešimas
-            if(!empty($success_message)){
-            ?>
-            <div class="alert alert-success">
-              <strong>Success!</strong> <?= $success_message ?>
-            </div>
-
-            <?php
-            }
-            ?>
+>>>>>>> 32a90620eef3770be565743a6912a65fc6ee425f
 
 
-
-
-            <div class="form-group">
-              <label for="fname">Vardas:</label>
-              <input type="text" class="form-control" name="fname" id="fname" required="required" maxlength="80">
-            </div>
-
-            <div class="form-group">
-              <label for="lname">Pavardė:</label>
-              <input type="text" class="form-control" name="lname" id="lname" required="required" maxlength="80">
-            </div>
-
-            <div class="form-group">
-              <label for="email">Pašto adresas:</label>
-              <input type="email" class="form-control" name="email" id="email" required="required" maxlength="80">
-            </div>
-           
-            <div class="form-group">
-            <label for="text">Artimiausios mokymų datos:</label><br>
-
-            
-            <p>2020-05-05</p>
-            <p>2020-07-05</p>
-            <p>2020-09-05</p>
-            
-
-          </div>
-           
-         
-
-            
-
-
-
-            <div class="form-group">
-              <label for="phone">Telefono numeris:</label>
-              <p>+370 <input  type="phone" class="form-control" name="phone" id="phone" required="required" maxlength="8"></p>
-            </div>
-
-            
-            
-            
-            
-
-          
-            <br><button type="submit" href="message.php" name="btnsignup" class="btn btn-default">Registruotis<br></button>
-           <?php
-            if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['fname'];
-    $last_name = $_POST['lname'];
-    $subject = "registracija i konsultacijas";
-   
-    $message = $first_name . " " . $last_name . " nori uzsiregistruoti i mokymus:" . "\n\n" . $_POST['message'];
-     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-    ?>
-        
-
-
-            <br><script src='https://www.google.com/recaptcha/api.js?hl=es'></script>
-<br><div class="g-recaptcha" data-sitekey="6Ldk7e4UAAAAAGyDs2ieQuTS9h8rDgYiKppHGmth"></div>
-           
-          </form>
-          
-        </div>
-
-        
-     </div>
+<div class="container">
+    <div style="text-align:center">
+        <h2>Registracija į mokymus</h2>
+       
     </div>
-    <?php
-    include "footer.php";
-    ?>
-  </body>
+<html>
+<head>
+<title>PHP Contact Form</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+    <div class="form-container">
+        <form name="frmContact" id="" frmContact"" method="post"
+            action="" enctype="multipart/form-data"
+            onsubmit="return validateContactForm()">
+
+            <div class="input-row">
+                <label style="padding-top: 20px;">Vardas</label> <span
+                    id="userName-info" class="info"></span><br /> <input
+                    type="text" class="input-field" name="userName"
+                    id="userName" />
+            </div>
+            <div class="input-row">
+                <label style="padding-top: 20px;">Pavardė</label> <span
+                    id="lname-info" class="info"></span><br /> <input
+                    type="text" class="input-field" name="lname"
+                    id="lname" />
+            </div>
+<<<<<<< HEAD
+            <div class="input-row">
+                <label>Paštas</label> <span id="userEmail-info"
+                    class="info"></span><br /> <input type="text"
+                    class="input-field" name="userEmail" id="userEmail" />
+            </div>
+            <div class="input-row">
+                <label>Tel. nr.</label> <span id="Privalomas užpildyti laukas"
+                    class="info"></span><br /> <input type="number"
+                    class="input-field" name="subject" id="subject" />
+            </div>
+            <div class="input-row">
+                <label>Pasirinkite mokymu data:</label> <span id="userMessage-info"
+                    class="info"></span><br />
+
+                    
+                <textarea name="content" id="content"
+                    class="input-field" cols="60" rows="6"></textarea>
+            </div>
+            <div>
+                <input type="submit" name="send" class="btn-submit"
+                    value="Registruotis" />
+
+                <div id="statusMessage"> 
+                        <?php
+                        if (! empty($message)) {
+                            ?>
+                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"
+        type="text/javascript"></script>
+    <script type="text/javascript">
+        function validateContactForm() {
+            var valid = true;
+
+            $(".info").html("");
+            $(".input-field").css('border', '#e0dfdf 1px solid');
+            var userName = $("#userName").val();
+            var lname = $("#lname").val();
+            var userEmail = $("#userEmail").val();
+            var subject = $("#subject").val();
+            var content = $("#content").val();
+            
+            if (userName == "") {
+                $("#userName-info").html("Privalomas užpildyti laukas");
+                $("#userName").css('border', '#e66262 1px solid');
+                valid = false;
+                
+            }
+            if (lname == "") {
+                $("#lname-info").html("Privalomas užpildyti laukas.");
+                $("#lname").css('border', '#e66262 1px solid');
+                valid = false;
+                
+            }
+            if (userEmail == "") {
+                $("#userEmail-info").html("Privalomas užpildyti laukas.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+            {
+                $("#userEmail-info").html("netinkamai įvestas pašto adresas.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+
+            if (subject == "") {
+                $("#subject-info").html("Privalomas užpildyti laukas.");
+                $("#subject").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (content == "") {
+                $("#userMessage-info").html("Privalomas užpildyti laukas.");
+                $("#content").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            return valid;
+            
+        }
+        
+</script>
+
+</body>
 </html>
+
+<br><?php include "footer.php" ?>
